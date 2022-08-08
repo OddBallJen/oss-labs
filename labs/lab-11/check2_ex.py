24b22137bd65c171105ca90fd7c5650b81ee91a7
Jenny S <57297201+OddBallJen@users.noreply.github.com>
2022-08-08 19:15:25 -0400

    images

--- labs/lab-11/check2_ex.py
+++ labs/lab-11/check2_ex.py
@@ -1,11 +1,13 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
 
+
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
-print(tf.__version__)
+# print(tf.__version__)
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
@@ -16,6 +18,7 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 train_images.shape
 
+print("type is: " + str(type(train_images))+"\n")
 len(train_labels)
 
 train_labels
@@ -42,7 +45,7 @@ for i in range(25):
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.show()
 
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -58,7 +61,7 @@ model.fit(train_images, train_labels, epochs=10)
 
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print('\nTest accuracy:', test_acc)
+# print('\nTest accuracy:', test_acc)
 
 # -- training complete -- 
 
@@ -112,7 +115,7 @@ plt.subplot(1,2,1)
 plot_image(i, predictions[i], test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions[i],  test_labels)
-plt.show()
+# plt.show()
 
 i = 12
 plt.figure(figsize=(6,3))
@@ -120,7 +123,7 @@ plt.subplot(1,2,1)
 plot_image(i, predictions[i], test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions[i],  test_labels)
-plt.show()
+# plt.show()
 
 # Plot the first X test images, their predicted labels, and the true labels.
 # Color correct predictions in blue and incorrect predictions in red.
@@ -138,22 +141,72 @@ plt.show()
 
 
 # Grab an image from the test dataset.
-img = test_images[1]
+#img = test_images[1]
 
-print(img.shape)
+#print(img.shape)
 
 # Add the image to a batch where it's the only member.
-img = (np.expand_dims(img,0))
+#img = (np.expand_dims(img,0))
 
-print(img.shape)
+#print(img.shape)
 
-predictions_single = probability_model.predict(img)
+#predictions_single = probability_model.predict(img)
 
-print(predictions_single)
+#print(predictions_single)
 
-plot_value_array(1, predictions_single[0], test_labels)
+#plot_value_array(1, predictions_single[0], test_labels)
+#_ = plt.xticks(range(10), class_names, rotation=45)
+#plt.show()
+
+#np.argmax(predictions_single[0])
+
+######################### Checkpoint 3 ####################################
+
+img1 = Image.open("clothes1_edited.jpg")
+img2 = Image.open("clothes2_edited.jpg")
+img3 = Image.open("clothes3_edited.jpg")
+
+img1 = np.array(img1)
+img2 = np.array(img2)
+img3 = np.array(img3)
+
+img1 = img1/255.0
+img2 = img2/255.0
+img3 = img3/255.0
+
+np_img1 = np.expand_dims(img1, 0)
+np_img2 = np.expand_dims(img2, 0)
+np_img3 = np.expand_dims(img3, 0)
+
+print ("image 1\n")
+print(np_img1.shape)
+predictions_single1 = probability_model.predict(np_img1)
+print(predictions_single1)
+plot_value_array(1, predictions_single1[0], test_labels)
 _ = plt.xticks(range(10), class_names, rotation=45)
 plt.show()
+print(np.argmax(predictions_single1[0]))
+print("\n")
 
-np.argmax(predictions_single[0])
+print ("image 2\n")
+print(np_img2.shape)
+predictions_single2 = probability_model.predict(np_img2)
+print(predictions_single2)
+plot_value_array(1, predictions_single2[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+print(np.argmax(predictions_single2[0]))
+print("\n")
+
+print ("image 3\n")
+print(np_img3.shape)
+predictions_single3 = probability_model.predict(np_img3)
+print(predictions_single3)
+plot_value_array(1, predictions_single3[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+print(np.argmax(predictions_single3[0]))
+
+
+
 
